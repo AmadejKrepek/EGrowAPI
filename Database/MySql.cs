1490adfc11fad67754de80181e10b214d69a87f9
Filip Strajnar <filip.strajnar@gmail.com>
2022-03-09 13:44:48 +0100

    user

--- Database/MySql.cs
+++ Database/MySql.cs
@@ -13,5 +13,7 @@ namespace Database
             optionsBuilder.UseMySQL(
                 @"server=db.funsw.us;database=db;user=db;password=fc16c58e9e221bf980874e43cc837824a4d75a7768a3b5073fef2ad8393a05ad");
         }
+
+        public DbSet<Models.User> User { get; set; }
     }
 }
---
+++ Migrations/20220309124441_MigrationName13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EGrowAPI.Migrations
+{
+    public partial class MigrationName13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
