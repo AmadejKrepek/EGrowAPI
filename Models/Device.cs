3c659fe73474ab878849c5012a61a7a981cfc5aa
Filip Strajnar <filip.strajnar@gmail.com>
2022-03-10 17:16:20 +0100

    refactoring IDs and other attributes

--- Models/Device.cs
+++ Models/Device.cs
@@ -7,6 +7,7 @@ namespace Models
     public class Device
     {
         [Key]
+        public int Id { get; set; }
         public string DeviceGuid { get; set; }
         public int WaterTankLevel { get; set; }
         public int FertilizerLevel { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -7,9 +7,11 @@ namespace Models
     public class User
     {
         [Key]
+        public int Id { get; set; }
         public string UserGuid { get; set; } = Guid.NewGuid().ToString();
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
         public List<Device> Devices { get; set; }
         public DateTime UserRegistration { get; set; }
     }
